26f8d2d77a32d769f98184c40ea7f533d2395512
George Joseph <71743801+GeorgeJoseph5000@users.noreply.github.com>
2024-04-23 14:03:19 +0200

    Update bit_utilies.h

--- Headers/Services/bit_utilies.h
+++ Headers/Services/bit_utilies.h
@@ -1,13 +1,16 @@
 #ifndef Bit_Utilies
 #define Bit_Utilies
 
-#define GET_BIT(reg1,bit)   ((reg1>>bit)&1)
-#define GET_REG(reg)        (reg&0xff)
+#define READ_REG(reg)        (reg&0xff)
 
 #define SET(reg,val)        reg |= val
-#define SET_BIT(reg,val)    reg |= (1<<val)
 
-#define CLR(reg,val)        reg &=~val
-#define CLR_BIT(reg,val)    reg &=~(1<<val)
+#define CLEAR(reg,val)        reg &=~val
+
+
+#define SET_BIT(REG,BIT_POSN)       (REG |= (1 << BIT_POSN))
+#define CLEAR_BIT(REG,BIT_POSN)     (REG &= ~(1 << BIT_POSN))
+#define TOGGLE_BIT(REG,BIT_POSN)    (REG ^= (1 << BIT_POSN))
+#define READ_BIT(REG,BIT_POSN)      ( (REG>>BIT_POSN) & (uint8_t)1 )
 
 #endif
