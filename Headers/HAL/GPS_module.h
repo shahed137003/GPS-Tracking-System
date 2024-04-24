914455abce1bba54013fceef39f4d5d49f46221b
shahed137003 <ahmedmohamed.aa777@gmail.com>
2024-04-24 03:03:10 +0200

    adding gps.h add there will have some modifications

---
+++ Headers/HAL/GPS_module.h
@@ -0,0 +1,16 @@
+#ifndef GPS
+#define GPS
+# define PI 3.141592653589793238
+# define EarthRadius  6371000
+#include <Math.h>
+#include <String.h>
+#include <stdlib.h>
+
+
+
+void GPS_read(void);
+
+
+void GPS_output_format();
+
+#endif
