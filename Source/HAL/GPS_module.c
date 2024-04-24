c948d65729f52c54a7d9fdc731fb981fa06b50e3
Nouran-Salah <144157011+Nouran-Salah@users.noreply.github.com>
2024-04-24 16:11:35 +0300

    Update GPS_module.c

--- Source/HAL/GPS_module.c
+++ Source/HAL/GPS_module.c
@@ -1,95 +1,150 @@
 # include "../../Headers/HAL/GPS.h"
-
 # include "../../Headers/MCAL/UART.h"
-
 # include <string.h>
-
 # include <stdlib.h>
-
+#include <stdio.h>
 # include <math.h>
 
-
-
-char GPS_module[80]; // this is the array that holds the output char from the GPS_module
-char log_name[]="$GPRMC, ";
-char logElements[12][20];
+char message[]="$GPRMC, 203522.00, A, 5109.0262308, S, 11401.8407342, E, 0.004, 133. 4, 130522, 0.0, E, D*2B";
+ char logElements[12][20];
 float currLong;
 float currLatit;
-char * pointer; // the pointer to store the addresof the first element in the array of each sepatrate element
+char * pointer; 
+char GPS_module[100]="";
+char log_name[]="$GPRMC, ";
+char recievedChar[100];  
 
-char recievedChar;
-void GPS_read(void){
-// first i will check the validity of the data to be recieved 
-	// i can check by the log name or any part in log 
 
-char flag=1;
-	char i=0;
-	do{
-		recievedChar=getChar();
-flag=1;
-	for(char i=0;i<7;i++){
-		// the falg will be always 1 in case all chars are matched     
-	
-	if(log_name[i]!=getChar())
-	{
-	flag=0;
-		break;
-	
-	}
-	}
-	}while(flag==0);
-
+	void GPS_read(){                                 //this function will only work when Uart reads data
+int flag=1;
+	int i;
+	  for( i=0;i<100;i++){  
+	 GPS_module[i]=message[i];    			// the flag will be always 1 in case all chars are matched 
+			  }
+    for(i=0;i<7;i++)		
+	   if(log_name[i]!= GPS_module[i])
+	     {
+	       flag=0;
+	      	break;
+	     }
+   	}
+   	
 //then we are sure that we are recieving the correct data 
 
-	strcpy(GPS_module,""); // for init the array
-	
-	
-	
-
-	do{
-	char filler=0;
-	recievedChar=getChar();
-	GPS_module[filler++]=recievedChar;
-	}while(recievedChar!='*');	
-		
-}
 // for formating the gps module output:
- 
+		
 void GPS_output_format(){
 	char numOfGPSElements=0;
+	pointer=strtok( message,", ");
 do{
-pointer=strtok(GPS_module,",");
-
-strcpy(logElements[numOfGPSElements],GPS_module);
-	pointer=strtok(NULL,",");
+strcpy(logElements[numOfGPSElements], pointer);
+	pointer=strtok(NULL,", ");
 	numOfGPSElements++;
 }while(pointer!=NULL);
 
-//then we will get the current long and lat :)
+/////////////////////////
 
-// here we check the validity of the string formated gps_module
-if(strcmp(logElements[0],"A")==0)
+(logElements[3]);
+////then we will get the current long and lat :)
+
+// //here we check the validity of the string formated gps_module
+if(strcmp(logElements[2],"A")==0)
 {
 
-
-// to get the current long:)
-if(strcmp(logElements[3],"N")==0)
-	currLong=atof(logElements[2]);
-else if(strcmp(logElements[3],"S")==0)
- currLong=-atof(logElements[2]);
-
- //to get the current latitude:)
-if(strcmp(logElements[5],"E")==0)
-	currLatit=atof(logElements[4]);
-else if(strcmp(logElements[5],"W")==0)
- currLatit=-atof(logElements[4]);
-
-
+//// to get the current long:)
+if(strcmp(logElements[4],"N")==0){
+	currLatit=atof(logElements[3]);
+printf("currLatit: %f \n",currLatit);	
+}
+else if(strcmp(logElements[4],"S")==0){
+ currLatit=-atof(logElements[3]);
+printf("currLatit: %f \n",currLatit);	
+    
 }
 
-
+// //to get the current latitude:)
+if(strcmp(logElements[6],"E")==0){
+	currLong=atof(logElements[5]);
+	printf("currLong: %f",currLong);	
+}
+else if(strcmp(logElements[6],"W")==0){
+ currLong=-atof(logElements[5]);
+printf("currLong: %f",currLong);	
 }
 
+}
+}
+
+//-------------------------------------------------------------------------------------------------ANOTHER_CODE---------------------------------------------------------------------------------------------//
+//   char message[]="$GPRMC, 203522.00, A, 5109.0262308, N, 11401.8407342, W, 0.004, 133. 4, 130522, 0.0, E, D*2B";
+// char* logElements[100];
+// char log_name[]="$GPRMC, ";
+// float currLong,directed_currLong ;
+// float currLatit,directed_currLatit;
+// char *pointer;
+// char recievedChar[100]; 
+
+// char* coordinates[2];
+
+// 	void GPS_read(){                        
+// int flag=1;
+// 	int i;
+// 	  for( i=0;i<100;i++){  
+// 			  recievedChar[i]=message[i];    		
+// 			  }
+//     for(i=0;i<7;i++)		
+// 	   if(log_name[i]!=recievedChar[i])
+// 	     {
+// 	       flag=0;
+// 	      	break;
+// 	     }
+//   	}
+
+ 
+// void GPS_output_format(){
+//     int counter=0;
+//     int valid=0;
+//   pointer=strtok(message," ,");
+     
+//     while (pointer) {
+//         if((strcmp(pointer,"$GPRMC")==0))
+//             counter=0;
+//       if(counter==2&&(strcmp(pointer," A")==0))   
+//           valid =1;
+//       if (counter==3&&valid ==1) 
+//              currLatit=atof(pointer);
+//              if (counter == 4&&valid==1) {
+//                 if (strcmp(pointer, " N") == 0) 
+//                   directed_currLatit= currLatit ;
+//                     else if (strcmp(pointer, " S") == 0) 
+//                       directed_currLatit=-currLatit ;
+//                  }
+
+//       if (counter==5&&valid==1)
+//             currLong=atof(pointer);
+//             //to get the current latitude:
+//               if (counter == 6&&valid==1) {
+//                  if (strcmp(pointer, " E") == 0) 
+//                   directed_currLong=currLong;
+//                      else if (strcmp(pointer, " W") == 0) 
+//                       directed_currLong=-currLong;
+//                  }
+//         counter++;
+//       pointer=strtok(NULL, ",");
+      
+//     }
+//         printf("CurrLatit: %f\n",directed_currLatit);
+//         printf("CurrLong:%f\n",directed_currLong);
+
+// }
+
+
+
+
+ int main(){
+   GPS_read();
+   GPS_output_format();
+ }
 
 
 
