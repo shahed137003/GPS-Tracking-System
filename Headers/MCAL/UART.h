ba9a039d55e40eec037c36287b86b8a36f193a79
(ENG) <167337048+Toqa-Gamal@users.noreply.github.com>
2024-04-26 13:25:12 +0300

    Update UART.h

--- Headers/MCAL/UART.h
+++ Headers/MCAL/UART.h
@@ -14,29 +14,17 @@
 #include "../tm4c123gh6pm.h"
 
 /******** UART0 **********/
-void UART0_Init(void); //initialization for UART0
+void UART0_Ini(void); //initialization for UART0
 
-unsigned char UART0_InChar(void); //to Receive the data
+char CharIn0(void); //to receive data
 
-unsigned char UART0_InCharNonBlocking(void);
-
-void UART0_OutChar(unsigned char data); //to transmit the data
+void CharOut0(char data); //to transmit the data
 
 /******** UART1 **********/
-void UART1_Init(void);  //initialization for UART1
+void UART1_Ini(void);  //initialization for UART1
 
-unsigned char UART1_InChar(void); //to Receive the data
+unsigned char CharIn1(void); //to Receive the data
 
-unsigned char UART1_InCharNonBlocking(void); 
+void CharOut1(char data); //to transmit the data
 
-void UART1_OutChar(unsigned char data); //to transmit the data
-
-/******** UART2 **********/
-void UART2_Init(void); //initialization for UART2
-
-unsigned char UART2_InChar(void); //to Receive the data
-
-unsigned char UART2_InCharNonBlocking(void);
-
-void UART2_OutChar(unsigned char data); //to transmit the data
 #endif
